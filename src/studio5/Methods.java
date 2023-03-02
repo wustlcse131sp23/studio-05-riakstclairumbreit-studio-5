dc524fabbadf491acf645e48e989ea1e684febaf
andersonstclair <andersonstclair@localhost>
2023-03-02 12:45:16 -0600

    nosotros es complete

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,7 +14,7 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -35,17 +35,20 @@ public class Methods {
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
 
-		
+		StdDraw.setPenColor(0,109,219);
+		StdDraw.filledCircle(x, y, (3.0/4.0) * radius);
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
 
-		
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledCircle(x, y, 0.5 * radius);
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
 
-		
+		StdDraw.setPenColor(225,225,109);
+		StdDraw.filledCircle(x, y, 0.25 * radius);
 	}
 
 	/**
@@ -60,10 +63,22 @@ public class Methods {
 	 *         characters in the source String with the replacement String
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
+		char[] characterArray = source.toCharArray(); 
 		String result = "";
+		for(int i = 0; i < characterArray.length; i++) {
+			if (characterArray[i] == target) {
+				result = result + replacement;
+			}
+			else {
+				result = result + characterArray[i];
+			}
+		}
 		// TODO: Finish this method
 		
 		return result;
+	
+		// TODO Auto-generated method stub
+		
 	}
 
 	/**
@@ -74,6 +89,9 @@ public class Methods {
 	 */
 	public static int arraySum(int[] values) {
 		int sum = 0;
+		for(int i = 0; i < values.length; i ++) {
+			sum = sum + values[i];
+		}
 		// FIXME: Compute the sum of the values in an array
 		
 		return sum;
@@ -87,7 +105,11 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length];
+		for(int i = 0; i < values.length; i++) {
+			values[i] = value;
+		}
+		// FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
 
 		
